aedc7e45278e5e47987fc1b306c01d942e4350ea
0xabandon <D2092494182@outlook.com>
2024-05-29 06:54:48 +0800

    主程序

--- game_main.py
+++ game_main.py
@@ -11,11 +11,18 @@ class PlaneGame(object):
         self.clock = pygame.time.Clock()
         #调用私有方法，精灵和精灵组的创建
         self.__creat_sprites()
+        #设置创建敌机
+        pygame.time.set_timer(ENEMY_EVENT,1000)
     def __creat_sprites(self):
         #创建背景精灵和精灵组
         bg1 = Background()
         bg2 = Background(True)
         self.back_group = pygame.sprite.Group(bg1,bg2)
+        #创建英雄精灵和精灵组
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
+        #创建敌机精灵组
+        self.enemy_group = pygame.sprite.Group()
     def start_game(self):
         print("游戏开始...")
         #游戏循环
@@ -35,11 +42,38 @@ class PlaneGame(object):
             # 判断是否为退出
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
+            #创建敌机
+            elif event.type == ENEMY_EVENT:
+                #print("敌机出场...")
+                # 创建敌机精灵
+                enemy = Enemy()
+                #将敌机精灵添加到敌机精灵组
+                self.enemy_group.add(enemy)
+        #使用键盘提供的方法获取键盘按键(按键元组)
+        keys_pressed = pygame.key.get_pressed()
+        #判断元组中对应的按键索引值 1
+        if keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -2
+        elif keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed = 2
+        elif keys_pressed[pygame.K_UP]:
+            self.hero.speed_2 = -2
+        elif keys_pressed[pygame.K_DOWN]:
+            self.hero.speed_2 = 2
+        else:
+            self.hero.speed = 0
+            self.hero.speed_2 = 0
     def __check_collide(self):
         pass
     def __update_sprites(self):
         self.back_group.update()
         self.back_group.draw(self.windows)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.windows)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.windows)
     #静态方法
     @staticmethod
     def __game_over():

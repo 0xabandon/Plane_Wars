d444f1b4e40006595674eead78c0b226792f901a
0xabandon <D2092494182@outlook.com>
2024-05-29 08:43:53 +0800

    精灵程序

--- game_sprites.py
+++ game_sprites.py
@@ -7,6 +7,8 @@ WINDOWS_RECT = pygame.Rect(0,0,512,768)
 FRAME_PER_SEC = 60
 #创建敌机的定时器常量
 ENEMY_EVENT = pygame.USEREVENT
+#英雄发射子弹事件
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 class GameSprite(pygame.sprite.Sprite):
     """游戏精灵"""
@@ -31,6 +33,7 @@ class Background(GameSprite):
         #判断交替图像，设置初始位置
         if is_alt:
             self.rect.y = -self.rect.height
+
     def update(self):
         #调用父类的方法实现
         super().update()
@@ -70,6 +73,8 @@ class Hero(GameSprite):
         #指定英雄初始位置
         self.rect.centerx = WINDOWS_RECT.centerx
         self.rect.bottom = WINDOWS_RECT.bottom
+        #创建子弹的精灵组
+        self.bullets = pygame.sprite.Group()
 
     def update(self):
         #英雄在水平方向移动
@@ -85,3 +90,30 @@ class Hero(GameSprite):
             self.rect.top = WINDOWS_RECT.top
         elif self.rect.bottom > WINDOWS_RECT.bottom:
             self.rect.bottom = WINDOWS_RECT.bottom
+
+    def fire(self):
+        for i in (0,1,2):
+            #创建子弹精灵
+            bullet = Bullet()
+            #设置精灵的位置
+            bullet.rect.bottom = self.rect.y - i * 20
+            bullet.rect.centerx = self.rect.centerx
+            #将精灵添加到精灵组
+            self.bullets.add(bullet)
+
+class Bullet(GameSprite):
+    """子弹精灵"""
+    def __init__(self):
+        #调用父类方法，设置子弹图片和速度
+        super().__init__("./图像素材/bullet/bullet.png",speed=-2)
+
+    def update(self):
+        # 调用父类方法，让子弹沿垂直飞行
+        super().update()
+        # 判断子弹是否飞出屏幕，是则从精灵组删除敌机
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        #print("子弹被摧毁%s"%self.rect)
+        pass
